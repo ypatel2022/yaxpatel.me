a381c4026cc281ac550234af866cb5cd78dd741f
ypatel2022 <yaxpatel2004@gmail.com>
2023-04-02 21:20:04 -0400

    update mobile contact button sizing

--- components/MobileContactButton.tsx
+++ components/MobileContactButton.tsx
@@ -8,7 +8,7 @@ export default function MobileContactButton() {
   return (
     <Link
       href="/#contact"
-      className="my-2 flex max-w-fit items-center justify-center overflow-hidden rounded-full bg-white px-5 py-2 leading-5 text-black"
+      className="my-2 flex max-w-fit items-center justify-center overflow-hidden rounded-full bg-white px-5 py-3 text-2xl leading-5 text-black"
     >
       <div className="flex w-full gap-4">
         <div className="flex items-center gap-2">Contact</div>
