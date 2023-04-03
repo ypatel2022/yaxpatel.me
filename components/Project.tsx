82d0dad7e998196674428e7e6e93636786db63b2
YaxP <yaxpatel2004@gmail.com>
2023-04-03 09:42:39 -0400

    fix text showing outside button

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,14 +1,39 @@
 import React, { useState } from 'react'
 import type { ProjectType } from '@/types'
+import Link from 'next/link'
+import ExternalLinkIcon from './icons/ExternalLinkIcon'
 
-function randomBackground() {
-  const colors = [
-    'bg-gradient-to-br from-black to-indigo-950',
-    'bg-gradient-to-br from-teal-400 via-teal-300 to-teal-400',
-  ]
+function ExternalLink({
+  children,
+  href,
+}: {
+  children: React.ReactNode
+  href: string
+}) {
+  return (
+    <Link
+      href={href}
+      className="group relative flex max-w-fit items-center justify-center gap-4 overflow-hidden rounded-full bg-gray-950 px-5 py-2 leading-5 text-[#fffdf3]"
+    >
+      <div className="w-full">
+        <div className="w-full transition-transform duration-300 ease-in-out group-hover:-translate-y-12">
+          <div className="flex items-center gap-2">{children}</div>
+        </div>
+        <div className="pointer-events-none absolute translate-y-2 select-none transition-transform duration-300 ease-in-out group-hover:-translate-y-5">
+          <div className="flex items-center gap-2">{children}</div>
+        </div>
+      </div>
 
-  return ''
-  return colors[Math.floor(Math.random() * colors.length)]
+      <div className="w-full">
+        <div className="w-full transition-transform duration-700 ease-in-out group-hover:-translate-y-12">
+          <ExternalLinkIcon />
+        </div>
+        <div className="pointer-events-none absolute translate-y-2 select-none transition-transform duration-700 ease-in-out group-hover:-translate-y-6">
+          <ExternalLinkIcon />
+        </div>
+      </div>
+    </Link>
+  )
 }
 
 export default function Project({
@@ -24,7 +49,7 @@ export default function Project({
     <div
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
-      className={`w-full px-10 text-neutral-900 transition-all hover:text-white xl:px-0 ${randomBackground()}`}
+      className="w-full px-10 text-neutral-900 transition-all hover:text-white xl:px-0"
       style={{
         background: hover
           ? `url(/project-backgrounds/project-background-${index + 1}.png)`
@@ -33,7 +58,13 @@ export default function Project({
     >
       <div className="mx-auto grid w-full max-w-7xl grid-cols-8 items-center gap-4 py-8">
         <div className="wght-700 col-span-5 text-6xl">{data.title}</div>
-        <div className="col-span-3">{data.description}</div>
+        <div className="col-span-3">
+          {data.description}
+
+          <div className="">
+            <ExternalLink href="/">Link</ExternalLink>
+          </div>
+        </div>
       </div>
     </div>
   )
